0135e61c7b4d3a6ea3fec13331f08aa230d537e9
yyu-kairos <yuyemin@kairosmaterials.com>
2026-09-18 21:48:06 +0000

    Update Kairos Materials logo to SVG

--- site/tests/lib/labels.test.ts
+++ site/tests/lib/labels.test.ts
@@ -120,7 +120,7 @@ describe(`get_org_logo`, () => {
       ),
     ],
     [`DeePMD`, src_logo(`DeePMD`, `/logos/deepmd.svg`)],
-    [`Kairos Materials`, src_logo(`Kairos Materials`, `/logos/kairos-materials.png`)],
+    [`Kairos Materials`, src_logo(`Kairos Materials`, `/logos/kairos-materials.svg`)],
   ])(`returns correct logo data for '%s'`, (input: string, expected: unknown) => {
     expect(get_org_logo(input)).toStrictEqual(expected)
   })
